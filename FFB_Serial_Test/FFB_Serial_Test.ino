fabeacc47611e4e89f6a774957530a999a023fc3
Hamnavoeper <hamnavoeper@gmail.com>
2021-03-25 17:29:23 +0000

    Added Debug Tools and structured for arduino IDE

---
+++ FFB_Serial_Test/FFB_Serial_Test.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup()
+{
+  analogReference(5);
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Native USB only
+  }
+
+
+
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(38400);
+  mySerial.println("Hello, world?");
+}
+
+void loop() // run over and over
+{
+ int xAxis = analogRead(A1);
+ int yAxis = analogRead(A2); 
+
+  if (mySerial.available())
+    Serial.println(" ");
+    Serial.println(xAxis);
+    Serial.println(" ");
+    Serial.println(yAxis);
+    delay(50);
+}
